9118d2aace513601bdae51fe2c2f354cc7d2ef85
Yusx <18307207149.163.com>
2019-05-13 12:21:35 +0800

    kafka

--- kafka/src/main/java/com/yusx/mq/ConsumerReceive.java
+++ kafka/src/main/java/com/yusx/mq/ConsumerReceive.java
@@ -6,29 +6,19 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 
-import java.nio.Buffer;
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class ConsumerReceive {
 
     public static void main(String[] args) {
 
-        KafkaConsumer<String,String> kafkaConsumer = new KafkaConsumer<String, String>(PropertiesReader.readeProperties("producer.properties"));
-        kafkaConsumer.subscribe(Arrays.asList("mytopic"));
-        final int minBatchSize = 200;
-        List<ConsumerRecord<String, String>> buffer = new ArrayList<>();
-        while (true){
-            ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofMillis(100));
-            for (ConsumerRecord<String, String> receive:consumerRecords){
-                buffer.add(receive);
-            }
-            if(buffer.size() >= minBatchSize){
-                kafkaConsumer.commitSync();
-                buffer.clear();
-            }
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(PropertiesReader.readeProperties("consumer.properties"));
+        consumer.subscribe(Arrays.asList("mytopic"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records)
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
         }
     }
 }
